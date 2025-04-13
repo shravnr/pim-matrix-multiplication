3bf8ac5594cfff8b043d770802aa671f282647e7
mz86 <mz86@linux-ssh-05.ews.illinois.edu>
2025-04-13 00:44:48 -0500

    Parameters added

--- pkg/types.sv
+++ pkg/types.sv
@@ -3,7 +3,7 @@ package types;
     parameter int MATRIX_SIZE = 8;
     parameter int CHUNK_SIZE = MATRIX_SIZE/2;
 
-    parameter int LEN = 32; // Adress width
+    parameter int LEN = 32; // Adress width, original 10, changed to align with top_tb.sv
     parameter int WIDTH = 32; // Data width
     parameter int MEM_ELEMENTS = 1024;
 
